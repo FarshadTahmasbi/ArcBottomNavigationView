e5cd89afe8335622ad8f2cde5679cc23c6c83dda
Farshad <farshad.tmb@gmail.com>
2019-06-22 01:29:05 +0430

    making sample

--- app/src/main/java/com/androidisland/views/sample/MainActivity.kt
+++ app/src/main/java/com/androidisland/views/sample/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.androidisland.views.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.ViewGroup
-import androidx.core.content.res.ResourcesCompat
+import androidx.appcompat.app.AppCompatActivity
 import com.androidisland.views.ArcBottomNavigationView
-import com.androidisland.views.toPixel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,18 +11,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ViewGroup.LayoutParams.MATCH_PARENT
         toggle_btn.setOnClickListener {
             arcBottomNav.toggleState()
         }
-        arcBottomNav.setOnNavigationItemSelectedListener {
-            Log.d("test123", "selected!")
-            true
-        }
-        arcBottomNav.buttonClickListener = {
-            Log.d("test123", "Button clicked!")
+        arcBottomNav.apply {
+            setOnNavigationItemSelectedListener {
+                Log.d("test123", "selected!")
+                true
+            }
+            buttonClickListener = {
+                Log.d("test123", "Button clicked!")
 //            it.state = ArcBottomNavigationView.State.FLAT
 //            it.buttonMargin = 16.toPixel()
+            }
         }
 
         state_switch.setOnCheckedChangeListener { buttonView, isChecked ->
@@ -38,16 +35,6 @@ class MainActivity : AppCompatActivity() {
                 arcBottomNav.state = ArcBottomNavigationView.State.FLAT
 
             }
-        }
-
-        val m1 = bottomNav.menu.getItem(0)
-        val m2 = bottomNav.menu.getItem(1)
-        val m3 = bottomNav.menu.getItem(2)
-        val m4 = bottomNav.menu.getItem(3)
-        val new = bottomNav.menu.add(Menu.NONE, 0, 5, "New!").apply {
-            icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_home_black, theme)
-            this.isEnabled = false
-            this.isVisible = false
         }
     }
 }
